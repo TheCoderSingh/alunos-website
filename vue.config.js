256c46d0e126be3ef880d7e51a2741212569a2d1
Walter Galvão <walterbgneto@gmail.com>
2020-07-17 11:35:24 -0700

    Removed pre-render

--- vue.config.js
+++ vue.config.js
@@ -1,20 +1,10 @@
 module.exports = {
   pluginOptions: {
-    prerenderSpa: {
-      registry: undefined,
-      renderRoutes: [
-        '/'
-      ],
-      useRenderEvent: true,
-      headless: true,
-      onlyProduction: true
-    },
     quasar: {
       importStrategy: 'kebab',
       rtlSupport: false
     }
   },
-
   transpileDependencies: [
     'quasar'
   ]
